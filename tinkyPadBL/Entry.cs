e586de5fd078b3503e7d50bb65fc69c60b6fe6c7
chupika <floyd14@gmail.com>
2019-05-02 22:40:29 +0500

    Initialize asp.net core

---
+++ tinkyPadBL/Entry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tinkyPadBL
+{
+    public class Entry
+    {
+        public string Caption { get; set; }
+
+        public string Addition { get; set; }
+
+        public string Link { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
---
+++ tinkyPadBL/Pad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tinkyPadBL
+{
+    public class Pad
+    {
+        private List<EntriesPage> EntriesPages = new List<EntriesPage>();
+
+        public int ActivePageIndex { get; set; }
+
+        public int ActiveTaskIndex { get; set; }
+    }
+}

12be2495c52555b4ccef9e5323e91d65d0395030
chupika <floyd14@gmail.com>
2019-06-17 23:51:20 +0500

    Add TestPostgreSQL (test passed)

---
+++ Tests/TestDB/TestPostgreSQL.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLogic;
+using DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Tests.TestDB
+{
+    [TestClass]
+    public class TestPostgreSQL
+    {
+        [TestMethod]
+        public void CreateAndSaveEntries()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var entry0 = new Entry { Caption = "Entry0", Addition = "Addition0" };
+                var entry1 = new Entry { Caption = "Entry1", Addition = "Addition1" };
+
+                db.Entries.Add(entry0);
+                db.Entries.Add(entry1);
+                db.SaveChanges();
+
+                var entries = db.Entries.ToList();
+                Console.WriteLine("Entries list:");
+                foreach (var entry in entries)
+                {
+                    Console.WriteLine($"{entry.Caption} - {entry.Addition}");
+                }
+            }
+        }
+    }
+}

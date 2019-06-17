affaa8d86f6e986585d811999e6908e785caf6ab
chupika <floyd14@gmail.com>
2019-06-17 23:52:11 +0500

    ApplicationContext switch to Entries from Pads

--- DataAccessLayer/ApplicationContext.cs
+++ DataAccessLayer/ApplicationContext.cs
@@ -4,12 +4,12 @@
     using BusinessLogic;
 
     public class ApplicationContext : DbContext
-        {
-            public DbSet<Pad> Pads { get; set; }
+    {
+        public DbSet<Entry> Entries { get; set; }
 
-            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            {
-                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=password");
-            }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=padsdb;Username=postgres;Password=");
         }
+    }
 }

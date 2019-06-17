db15273205b0d89f6ed2deff1810ba6f0faaa810
chupika <floyd14@gmail.com>
2019-06-17 23:48:24 +0500

    Add Id and mark NotMapped Tags of Entry

--- BusinessLogic/Entry.cs
+++ BusinessLogic/Entry.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BusinessLogic
 {
     public class Entry
     {
+        public int Id { get; set; }
         public string Caption { get; set; }
 
         public string Addition { get; set; }
 
         public string Link { get; set; }
 
+        [NotMapped]
         public HashSet<string> Tags { get; set; }
 
         public bool IsDone { get; set; }

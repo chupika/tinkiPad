0b19e7e302f06f01b80b7ae2ce058d78d12ac683
chupika <floyd14@gmail.com>
2019-07-17 23:40:21 +0500

    Add two tests for Pad method GetActiveEntry()

---
+++ Tests/BusinessLogic/PadTests.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Tests.BusinessLogic
+{
+    [TestClass]
+    public class PadTests
+    {
+        [TestMethod]
+        public void GetActiveEntry_WhenActiveIndexWasBeenReseted_ReturnNull()
+        {
+            var pad = new Pad();
+            pad.Entries = new List<Entry>
+            {
+                new Entry(),
+                new Entry(),
+                new Entry(),
+                new Entry(),
+                new Entry(),
+            };
+
+            pad.ActiveEntryIndex = 3;
+            pad.ResetActiveTask();
+            Assert.IsNull(pad.GetActiveEntry());
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(10)]
+        public void GetActiveEntry_WhenActiveEntryIndexWasBeenSetInRangeOfEntriesCount_ReturnNotNull(int activeEntryIndex)
+        {
+            var pad = new Pad();
+            pad.Entries = new List<Entry>
+            {
+                new Entry(),
+                new Entry(),
+                new Entry(),
+                new Entry(),
+                new Entry(),
+            };
+
+            pad.ActiveEntryIndex = activeEntryIndex;
+            pad.ResetActiveTask();
+            Assert.IsNull(pad.GetActiveEntry());
+        }
+    }
+}

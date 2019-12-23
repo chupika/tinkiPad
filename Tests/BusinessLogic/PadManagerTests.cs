e8d01cba620a05490816b78dc32bc81487be45c5
chupika <floyd14@gmail.com>
2019-12-24 01:31:26 +0500

    add test CompleteTask_ThenActiveTaskIndexIsReseted

--- Tests/BusinessLogic/PadManagerTests.cs
+++ Tests/BusinessLogic/PadManagerTests.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Xunit;
+using BusinessLogic;
 using System;
 
 namespace Tests.BusinessLogic
 {
-    [TestClass]
     public class PadManagerTests
     {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        public void CompleteTask_ThenActiveTaskIndexIsReseted(int taskIndex)
+        {
+            var pad = PadFiller.GetFullPad();
+            var padManager = new PadManager(pad);
+            pad.ActiveTaskIndex = taskIndex;
+            padManager.CompleteTask();
+            Assert.Equal(-1, pad.ActiveTaskIndex);
+        }
+
         public void TurnPage_WhenPendingPagesAreLessThanTwo_RaiseException()
         {
             throw new NotImplementedException();
